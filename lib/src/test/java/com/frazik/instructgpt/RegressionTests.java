5cdf26c566876247286e5eccac888508a7d334e8
spookysleeper <spookysleeper@gmail.com>
2023-06-24 15:49:17 +0200

    Add new test that should fail.

--- lib/src/test/java/com/frazik/instructgpt/RegressionTests.java
+++ lib/src/test/java/com/frazik/instructgpt/RegressionTests.java
@@ -70,4 +70,20 @@ public class RegressionTests {
         assertNotNull(resp);
     }
 
+    @Test
+    void basicChatResult() {
+        String name = "Shop assistant";
+        String description = "an AI assistant that researches and finds the best car";
+
+        List<String> goals = new java.util.ArrayList<>();
+        goals.add("Search for the car on Google");
+        goals.add("Write the list of the top 5 best cars and their prices to a file");
+        goals.add("Summarize the pros and cons of each car and write it to a different file called 'cars-research.txt'");
+        goals.add("There will be no user assistance. Terminate once writing both files is complete.");
+
+        Agent agent = new Agent(name, description, goals, "gpt-3.5-turbo");
+        Response resp = agent.chat();
+        assertEquals(resp.getCommand(), "google_search");
+    }
+
 }
