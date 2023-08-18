6700f58d19e67aba3708b70bdce9d9ce7ea9c781
spookysleeper <spookysleeper@gmail.com>
2023-08-18 09:55:15 +0200

    Start on logging

--- lib/src/main/java/com/frazik/instructgpt/models/Model.java
+++ lib/src/main/java/com/frazik/instructgpt/models/Model.java
@@ -1,7 +1,9 @@
 package com.frazik.instructgpt.models;
 
+import com.frazik.instructgpt.logging.ChatLogger;
 import com.theokanning.openai.completion.chat.ChatMessage;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,4 +13,11 @@ public abstract class Model {
     public abstract int countTokens(List<Map<String, String>> messages);
 
     public abstract int getTokenLimit();
+
+    public String chat(List<ChatMessage> messages, Integer maxTokens, Double temperature, ChatLogger logger) {
+        String response = chat(messages, maxTokens, temperature);
+        logger.write(messages, response);
+        return response;
+    }
+
 }
